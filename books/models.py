5c889e2462d645c6d4dab9f12c32d67140f314a9
Andriy <an_mydel@gmail.com>
2023-08-07 21:06:54 +0300

    Add Author model

--- books/models.py
+++ books/models.py
@@ -2,13 +2,21 @@ from django.core import validators
 from django.db import models
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+
 class Book(models.Model):
     class Cover(models.TextChoices):
         HARD = "Hard"
         SOFT = "Soft"
 
     title = models.CharField(max_length=511)
-    author = models.CharField(max_length=511)
+    author = models.ManyToManyField(Author, related_name="books")
     cover = models.CharField(max_length=4, choices=Cover.choices)
     inventory = models.IntegerField(
         validators=[
