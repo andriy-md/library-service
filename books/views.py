dd44964f4704a4718bee5d1c3b65e2c3ecab6d75
Andriy <an_mydel@gmail.com>
2023-08-09 00:08:10 +0300

    Add search by title for Book Api

--- books/views.py
+++ books/views.py
@@ -16,6 +16,15 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all().prefetch_related("authors")
     permission_classes = [IsAdminOrReadOnly]
 
+    def get_queryset(self):
+        queryset = Book.objects.all().prefetch_related("authors")
+        title = self.request.query_params.get("title")
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        return queryset
+
     def get_serializer_class(self):
         if self.action in ("create", "update", "partial_update"):
             return BookCreateSerializer
