4e27e9d1295a1f63dafc3dc53558f0412f9821a2
Andriy <an_mydel@gmail.com>
2023-08-09 19:47:26 +0300

    Add BorrowingListRetrieveSerializer

---
+++ borrowings/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from books.serializers import BookListRetrieveSerializer
+from borrowings.models import Borrowing
+
+
+class BorrowingListRetrieveSerializer(serializers.ModelSerializer):
+    book = BookListRetrieveSerializer(read_only=True, many=False)
+
+    class Meta:
+        model = Borrowing
+        fields = [
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user"
+        ]
