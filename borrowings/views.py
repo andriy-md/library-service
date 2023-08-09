702002983f4da37cfa57196f0f2c49b8f33a9bb9
Andriy <an_mydel@gmail.com>
2023-08-09 20:11:40 +0300

    Add Borrowing endpoint (no Create yet)

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingListRetrieveSerializer
+
+
+class BorrowingViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    GenericViewSet
+):
+    queryset = Borrowing.objects.all().select_related("book", "user")
+    serializer_class = BorrowingListRetrieveSerializer
+
+    def get_queryset(self):
+        return Borrowing.objects.all().filter(user=self.request.user).select_related("book", "user")
