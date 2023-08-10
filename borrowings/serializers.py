a7297ef2e10ff6c4037e1fad6e2503fa4ba8bc8f
Andriy <an_mydel@gmail.com>
2023-08-10 19:28:49 +0300

    Add functionality to create Borrowing at endpoint

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -17,3 +17,16 @@ class BorrowingListRetrieveSerializer(serializers.ModelSerializer):
             "book",
             "user"
         ]
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = [
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user"
+        ]
--- borrowings/views.py
+++ borrowings/views.py
@@ -2,7 +2,7 @@ from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
 
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingListRetrieveSerializer
+from borrowings.serializers import BorrowingListRetrieveSerializer, BorrowingCreateSerializer
 
 
 class BorrowingViewSet(
@@ -10,10 +10,15 @@ class BorrowingViewSet(
     mixins.RetrieveModelMixin,
     mixins.UpdateModelMixin,
     mixins.DestroyModelMixin,
+    mixins.CreateModelMixin,
     GenericViewSet
 ):
     queryset = Borrowing.objects.all().select_related("book", "user")
-    serializer_class = BorrowingListRetrieveSerializer
 
     def get_queryset(self):
         return Borrowing.objects.all().filter(user=self.request.user).select_related("book", "user")
+
+    def get_serializer_class(self):
+        if self.action in ("create", "update", "partial_update"):
+            return BorrowingCreateSerializer
+        return BorrowingListRetrieveSerializer
