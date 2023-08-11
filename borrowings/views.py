50ea80394b85d850623ffc8f39bf3cd43a2e6e85
Andriy <an_mydel@gmail.com>
2023-08-11 22:46:47 +0300

    Add permission that only respective User or Admin may retrieve Borrowing

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
 
 from borrowings.models import Borrowing
+from borrowings.permissions import UserOrAdminDetail
 from borrowings.serializers import BorrowingListRetrieveSerializer, BorrowingCreateSerializer
 
 
@@ -21,5 +23,18 @@ class BorrowingViewSet(
             return BorrowingCreateSerializer
         return BorrowingListRetrieveSerializer
 
+    def get_permissions(self):
+        if self.action == "retrieve":
+            self.permission_classes = [UserOrAdminDetail]
+
+        return super(BorrowingViewSet, self).get_permissions()
+
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    def get_object(self):
+        obj = get_object_or_404(self.get_queryset(), pk=self.kwargs["pk"])
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+
