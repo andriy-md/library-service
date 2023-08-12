567f10da1b8d5aa6d4973554506c92be8a970dc1
Andriy <an_mydel@gmail.com>
2023-08-12 09:30:33 +0300

    Add tests for Admin User

--- borrowings/tests/test_borrowing_api.py
+++ borrowings/tests/test_borrowing_api.py
@@ -55,7 +55,7 @@ def borrowing_detail_url(pk: int):
     return reverse("borrowings:borrowing-detail", args=[pk])
 
 
-class AuthenticatedBorrowingBookApiTest(TestCase):
+class AuthenticatedBorrowingApiTest(TestCase):
     def setUp(self) -> None:
         self.client = APIClient()
         self.user = get_user_model().objects.create_user(
@@ -191,67 +191,98 @@ class AuthenticatedBorrowingBookApiTest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
+class AdminBorrowingApiTest(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            email="test_user@admin.com", password="qwer1234", is_staff=True
+        )
+        self.client.force_authenticate(self.user)
 
-# class AdminBookApiTest(TestCase):
-#     def setUp(self) -> None:
-#         self.client = APIClient()
-#         user = get_user_model().objects.create_user(
-#             email="admin_user@admin.com", password="qwer1234", is_staff=True
-#         )
-#         self.client.force_authenticate(user)
-#
-#     def test_create_book(self):
-#         create_sample_author()
-#         payload = {
-#             "title": "New Book",
-#             "authors": [1],
-#             "cover": "Soft",
-#             "inventory": 20,
-#             "daily_fee": 2
-#         }
-#         response = self.client.post(BOOK_URL, data=payload)
-#
-#         book = Book.objects.get(id=1)
-#
-#         payload.pop("authors")
-#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-#         for key in payload:
-#             self.assertEqual(getattr(book, key), payload[key])
-#
-#     def test_put_book(self):
-#         author = create_sample_author()
-#         book = create_sample_book()
-#         url = book_detail_url(book.id)
-#         payload = {
-#             "title": "New Book",
-#             "authors": [author.id],
-#             "cover": "Soft",
-#             "inventory": 20,
-#             "daily_fee": 2
-#         }
-#
-#         response = self.client.put(url, data=payload)
-#         book.refresh_from_db()
-#
-#         payload.pop("authors")
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         for key in payload:
-#             self.assertEqual(getattr(book, key), payload[key])
-#
-#     def test_patch_book(self):
-#         book = create_sample_book()
-#         url = book_detail_url(book.id)
-#         payload = {
-#             "title": "New Book",
-#             "cover": "Soft",
-#             "inventory": 20,
-#             "daily_fee": 2
-#         }
-#
-#         response = self.client.patch(url, data=payload)
-#         book.refresh_from_db()
-#
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         for key in payload:
-#             self.assertEqual(getattr(book, key), payload[key])
+    def test_list_borrowings(self):
+        create_sample_borrowing(user=self.user)
+        create_sample_borrowing(user=self.user)
+        another_user = get_user_model().objects.create_user(
+            email="another_user@admin.com", password="qwer1234", is_staff=False
+        )
+        create_sample_borrowing(user=another_user)
 
+        response = self.client.get(BORROWING_URL)
+        borrowings = Borrowing.objects.all()
+        serializer = BorrowingListRetrieveSerializer(borrowings, many=True)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["count"], 3)
+        self.assertEqual(response.data["results"], serializer.data)
+
+    def test_create_borrowing_success(self):
+        book = create_sample_book()
+        initial_inventory = book.inventory
+        payload = {
+            "expected_return_date": datetime.strftime(
+                date.today() + timedelta(days=2),
+                "%Y-%m-%d"
+            ),
+            "actual_return_date": datetime.strftime(
+                date.today() + timedelta(days=2),
+                "%Y-%m-%d"
+            ),
+            "book": book.id,
+        }
+
+        response = self.client.post(BORROWING_URL, data=payload)
+        borrowing = Borrowing.objects.get(id=1)
+        payload.pop("book")
+        book.refresh_from_db()
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        for key in payload:
+            self.assertEqual(str(getattr(borrowing, key)), payload[key])
+        self.assertEqual(book.inventory, initial_inventory - 1)
+
+    def test_raises_error_if_expected_return_date_not_after_borrow_date(self):
+        book = create_sample_book()
+        payload = {
+            "expected_return_date": datetime.strftime(
+                date.today(),
+                "%Y-%m-%d"
+            ),
+            "actual_return_date": datetime.strftime(
+                date.today() + timedelta(days=2),
+                "%Y-%m-%d"
+            ),
+            "book": book.id,
+        }
+
+        response = self.client.post(BORROWING_URL, data=payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_raises_error_if_actual_return_date_less_or_equals_borrow_date(self):
+        book = create_sample_book()
+        payload = {
+            "expected_return_date": datetime.strftime(
+                date.today() + timedelta(days=1),
+                "%Y-%m-%d"
+            ),
+            "actual_return_date": datetime.strftime(
+                date.today() - timedelta(days=1),
+                "%Y-%m-%d"
+            ),
+            "book": book.id,
+        }
+
+        response = self.client.post(BORROWING_URL, data=payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_borrowing_of_other_user(self):
+        another_user = get_user_model().objects.create_user(
+            email="another_user@user.com", password="qwer1234", is_staff=False
+        )
+        borrowing = create_sample_borrowing(user=another_user)
+        url = borrowing_detail_url(borrowing.id)
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
