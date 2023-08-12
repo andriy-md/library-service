88b3780f3ecba3b4490dce5f32f10dba8aeef319
Andriy <an_mydel@gmail.com>
2023-08-12 09:46:25 +0300

    Add actual_return_date may be blank and null

--- borrowings/models.py
+++ borrowings/models.py
@@ -18,7 +18,7 @@ def validate_book_inventory(value):
 class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
-    actual_return_date = models.DateField()
+    actual_return_date = models.DateField(blank=True, null=True)
     book = models.ForeignKey(
         Book,
         on_delete=models.CASCADE,
